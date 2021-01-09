f6219b7c39b6d2356c0474cb07b1bb39abb1d7bc
Cynthia Trujillo <developercynthia@gmail.com>
2021-01-09 15:57:19 -0600

    Videos and products

---
+++ assets/sheety-videos/main.js
@@ -0,0 +1,30 @@
+// Wait for page to load
+document.addEventListener('DOMContentLoaded', function (event) {
+	ready();
+})
+
+function ready() {
+	let url = 'https://api.sheety.co/138bb146309fa7b0d19c62e8deab22b2/youflix/videos';
+	fetch(url)
+		.then((response) => response.json())
+		.then(json => {
+			let videos = json.videos;
+			// Parse out YouTube ID from the URL, as we'll use that to get the thumbnail
+			videos.forEach(video => {
+				let regex = /watch\?v=([\w\d-]*)/;
+				video.youtubeID = video.url.match(regex)[1];
+			});
+
+			let featuredVideo = videos.find(video => {
+				return video.isFeatured;
+			});
+
+			let groupedVideos = _.groupBy(json.videos, 'category');
+			var template = Handlebars.compile(document.getElementById("app-template").innerHTML);
+			// Render items into Handlebars template, and set the html of the container element
+			document.getElementById('app').innerHTML = template({
+				featured: featuredVideo,
+				videos: groupedVideos
+			});
+		});
+}
